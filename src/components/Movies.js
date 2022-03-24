ac7f9d62234532891033ab6ef8e9fd8b2dcb7acd
Martin Cristina Darroux <martincrisdarr@gmail.com>
2022-03-23 23:30:57 -0300

    actualizacion segunda parte

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,22 +1,74 @@
+import React from 'react'
 import Kids from './type-movies/Kids'
 import Popular from './type-movies/Popular'
 import Rated from './type-movies/Rated'
+import MovieCard from './MovieCard'
+
 import {BrowserRouter,Routes,Route,Link} from "react-router-dom";
 function Movies() { 
+    const [movies, setMovies] = React.useState ([])
+    const [movieType, setMovieType] = React.useState("rated")
+    const [isLoading, setIsLoading] = React.useState(false)
+    const urlPopularity = '/discover/movie?sort_by=popularity.desc'
+    const kids = '/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc'
+    const rated = '/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22'
+
+    
+    const getKidsMovies = () =>{
+      fetch(`https://api.themoviedb.org/3${kids}&api_key=a77a471f25b126245b946660f886a046`)
+      .then((res) => res.json())
+      .then(data => {
+        setMovies(data.results)
+        setTimeout(() => {
+          setIsLoading(false)  
+        });
+        
+      })
+    }
+    const getPopularMovies = () =>{
+      fetch(`https://api.themoviedb.org/3${urlPopularity}&api_key=a77a471f25b126245b946660f886a046`)
+      .then((res) => res.json())
+      .then(data => {
+        setMovies(data.results)
+        setTimeout(() => {
+          setIsLoading(false)  
+        });
+      })
+    }
+    const getRatedMovies = () =>{
+      fetch(`https://api.themoviedb.org/3${rated}&api_key=a77a471f25b126245b946660f886a046`)
+      .then((res) => res.json())
+      .then(data => {
+        setMovies(data.results)
+        setTimeout(() => {
+          setIsLoading(false)  
+        });
+      })
+    }
+    React.useEffect(() => {
+      setIsLoading(true)
+      if(movieType === "rated") {
+        getRatedMovies()
+      }
+      if(movieType === "kids"){
+        getKidsMovies()
+      }
+      if(movieType === "popular") {
+        getPopularMovies()
+      }
+    }, [movieType])
     return (
       <div className='movieContainer'>
-      <BrowserRouter>
+
         <div className='movieRoutes'>
-          <Link className='movieLinks' to="/rated-movies">Rated</Link>
-          <Link className='movieLinks' to="/">Popular</Link>
-          <Link className='movieLinks' to="kids-movies">Kids</Link>
+          <span className='movieLinks' to="#" onClick={(e) => e.preventDefault() & setMovieType("rated")}>Rated</span>
+          <span className='movieLinks' to="#" onClick={(e) => e.preventDefault() & setMovieType("popular")}>Popular</span>
+          <span className='movieLinks' to="#" onClick={(e) => e.preventDefault() & setMovieType("kids")}>Kids</span>
         </div>
-        <Routes>
-          <Route path='/' element={<Popular />}></Route>
-          <Route path='/kids-movies' element={<Kids />}></Route>
-          <Route path='/rated-movies' element={<Rated />}></Route>
-        </Routes>
-      </BrowserRouter>
+         <div className='movies'>
+          { isLoading ? <span> ESTA CARGANDO </span> : 
+              movies.map((movie) => <MovieCard  key={movie.id} {...movie} />)}
+        </div>
       </div>
     )
   }
